62de8b176f75fc3d0ca19b09e2d08af68e93eece
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-08 18:03:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(greeting)
   if greeting != greeting.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif greeting == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
